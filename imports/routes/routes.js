221ae7eefab03ef45580b554ac707a08ac4f1871
Jason Manns <Jason.Manns@gmx.com>
2017-04-15 13:39:49 +0100

    Mark selected note in list

--- imports/routes/routes.js
+++ imports/routes/routes.js
@@ -1,4 +1,5 @@
 import { Meteor } from 'meteor/meteor'
+import { Session } from 'meteor/session'
 import React from 'react'
 import { Router, Route, browserHistory } from 'react-router'
 
@@ -9,14 +10,24 @@ import NotFound from '../ui/NotFound'
 
 const unauthenticatedPages = ['/', '/signup']
 const authenticatedPages = ['/dashboard']
+
 const onEnterPublicPage = () => {
   if (Meteor.userId()) {
     browserHistory.replace('/dashboard')
   }
 }
+
 const onEnterPrivatePage = () => {
   if (!Meteor.userId()) {
     browserHistory.replace('/')
+  }
+}
+
+const onEnterNotePage = (nextState) => {
+  if (!Meteor.userId()) {
+    browserHistory.replace('/')
+  } else {
+    Session.set('selectedNoteId', nextState.params.id)
   }
 }
 
@@ -38,7 +49,7 @@ export const routes = (
     <Route path='/' component={Login} onEnter={onEnterPublicPage} />
     <Route path='/signup' component={Signup} onEnter={onEnterPublicPage} />
     <Route path='/dashboard' component={Dashboard} onEnter={onEnterPrivatePage} />
-    <Route path='/dashboard/:id' component={Dashboard} onEnter={onEnterPrivatePage} />
+    <Route path='/dashboard/:id' component={Dashboard} onEnter={onEnterNotePage} />
     <Route path="*" component={NotFound} />
   </Router>
 )
--- imports/ui/NoteListItem.js
+++ imports/ui/NoteListItem.js
@@ -9,6 +9,7 @@ export const NoteListItem = (props) => {
       props.Session.set('selectedNoteId', props.note._id)
     }}>
       <h5>{ props.note.title || 'Untitled note'}</h5>
+      { props.note.selected ? 'Selected' : 'Not Selected'}
       <p>{ moment(props.note.updatedAt).format('DD/MM/YY') }</p>
     </div>
   )
